ff5a2a2097be1104e870ade43159cd853b06f75b
Reymond Caceres <reymond19890@gmail.com>
2022-05-24 20:52:44 -0400

    modificar el modelo -agregar order a la sql, migraciones. -agregar orden a las paginas

--- pages/models.py
+++ pages/models.py
@@ -8,6 +8,7 @@ class Page(models.Model):
     title = models.CharField(max_length=50, verbose_name="Titulo")     
     content = RichTextField(verbose_name="contenido")
     slug = models.CharField(unique=True, max_length=20, verbose_name="URL amigable")
+    order = models.IntegerField(default=0, verbose_name="Orden")
     visible = models.BooleanField(verbose_name="¿visible?") 
     create_at = models.DateTimeField(auto_now_add=True, verbose_name= "Creado el")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Actualizado el")
